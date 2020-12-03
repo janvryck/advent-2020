7505fc6c3eb4f3d1562e3220d00e290c9a62dc4f
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2020-12-03 08:29:49 +0100

    03: small readability enhancements

--- src/main/kotlin/be/janvanryckeghem/aoc2020/Day03.kt
+++ src/main/kotlin/be/janvanryckeghem/aoc2020/Day03.kt
@@ -1,12 +1,12 @@
 package be.janvanryckeghem.aoc2020
 
-class Day03(inputFile: String): Day(inputFile) {
+class Day03(inputFile: String) : Day(inputFile) {
     override val DAY: String = "Day 03"
 
     override val parseInput: (String) -> List<CharArray> = { input ->
         input.lines()
             .filter { s -> s.isNotEmpty() }
-            .map { it.toCharArray()}
+            .map { it.toCharArray() }
     }
 
     override fun solvePart1(input: String): Long {
@@ -16,7 +16,11 @@ class Day03(inputFile: String): Day(inputFile) {
 
     override fun solvePart2(input: String): Long {
         val map = parseInput(input)
-        return treesOnTrajectory(map, 1) * treesOnTrajectory(map, 3) * treesOnTrajectory(map, 5) * treesOnTrajectory(map, 7) * treesOnTrajectory(map, 1, 2)
+        return treesOnTrajectory(map, 1)
+            .times(treesOnTrajectory(map, 3))
+            .times(treesOnTrajectory(map, 5))
+            .times(treesOnTrajectory(map, 7))
+            .times(treesOnTrajectory(map, 1, 2))
     }
 
     private fun treesOnTrajectory(map: List<CharArray>, right: Int = 3, down: Int = 1): Long {
@@ -24,9 +28,8 @@ class Day03(inputFile: String): Day(inputFile) {
         var col = 0
         var trees: Long = 0
         for (row in map.indices step down) {
-            trees = if (map[row][col] == '#') trees.inc() else trees
-            col += right
-            col %= width
+            if (map[row][col] == '#') trees++
+            col = col.plus(right).rem(width)
         }
         return trees
     }
