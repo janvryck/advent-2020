6f2e3e5c2d88ae6b958f800cf24f8b59b600e727
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2020-12-04 11:39:48 +0100

    03: fix specs after refactor

--- src/test/kotlin/be/janvanryckeghem/aoc2020/Day03Test.kt
+++ src/test/kotlin/be/janvanryckeghem/aoc2020/Day03Test.kt
@@ -8,12 +8,12 @@ internal class Day03Test {
     @Test
     fun part1() {
         val solution = Day03("sample-d03").part1()
-        Assertions.assertThat(solution).isEqualTo(7)
+        Assertions.assertThat(solution).isEqualTo(7L)
     }
 
     @Test
     fun part2() {
         val solution = Day03("sample-d03").part2()
-        Assertions.assertThat(solution).isEqualTo(336)
+        Assertions.assertThat(solution).isEqualTo(336L)
     }
 }
