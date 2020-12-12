a6e6e6c5e700fd1464769ec9662d1d378ca8bd56
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2020-12-12 13:19:41 +0100

    12: refactor common logic

--- src/main/kotlin/be/janvanryckeghem/aoc2020/Day12.kt
+++ src/main/kotlin/be/janvanryckeghem/aoc2020/Day12.kt
@@ -2,9 +2,9 @@ package be.janvanryckeghem.aoc2020
 
 import kotlin.math.abs
 
-class Day12(inputFile:String = "d12"): Day(inputFile) {
+class Day12(inputFile: String = "d12") : Day(inputFile) {
     override val DAY: String = "DAY 12"
-    override val parseInput: (String) -> List<Instruction> = { it.lines().map(::toInstruction)}
+    override val parseInput: (String) -> List<Instruction> = { it.lines().map(::toInstruction) }
 
     override fun solvePart1(input: String): Number {
         val boat = Boat()
@@ -14,109 +14,77 @@ class Day12(inputFile:String = "d12"): Day(inputFile) {
     }
 
     override fun solvePart2(input: String): Number {
-        val boat = Boat()
+        val boat = Boat(Point(10, 1))
+        val useWaypoints = true
         parseInput(input)
-            .forEach { boat.moveWithWaypoints(it) }
+            .forEach { boat.move(it, useWaypoints) }
         return boat.manhattanDistance()
     }
 
-    data class Boat(var direction: Direction = Direction.EAST, var position: Point = Point()) {
-        var waypoint = Point(10, 1)
+    data class Boat(var direction: Point = Direction.EAST.point) {
+        var position: Point = Point()
 
-        fun move(instruction: Instruction) {
-            when(instruction.action) {
+        fun move(instruction: Instruction, waypoint: Boolean = false) {
+            when (instruction.action) {
                 Action.FORWARD -> position += direction * instruction.value
-                Action.NORTH -> position += Direction.NORTH * instruction.value
-                Action.EAST -> position += Direction.EAST * instruction.value
-                Action.SOUTH -> position += Direction.SOUTH * instruction.value
-                Action.WEST -> position += Direction.WEST * instruction.value
-                Action.LEFT -> when(instruction.value) {
-                    90 -> direction = direction.turnLeft()
-                    180 -> direction = direction.turnAround()
-                    270 -> direction = direction.turnRight()
+                Action.NORTH ->
+                    if (waypoint) direction += Direction.NORTH * instruction.value
+                    else position += Direction.NORTH * instruction.value
+                Action.EAST ->
+                    if (waypoint) direction += Direction.EAST * instruction.value
+                    else position += Direction.EAST * instruction.value
+                Action.SOUTH ->
+                    if (waypoint) direction += Direction.SOUTH * instruction.value
+                    else position += Direction.SOUTH * instruction.value
+                Action.WEST ->
+                    if (waypoint) direction += Direction.WEST * instruction.value
+                    else position += Direction.WEST * instruction.value
+                Action.LEFT -> direction = when (instruction.value) {
+                    90 -> direction.turnLeft()
+                    180 -> direction.turnAround()
+                    270 -> direction.turnRight()
                     else -> throw IllegalStateException("Could not turn left by ${instruction.value}°")
                 }
-                Action.RIGHT -> when(instruction.value) {
-                    90 -> direction = direction.turnRight()
-                    180 -> direction = direction.turnAround()
-                    270 -> direction = direction.turnLeft()
+                Action.RIGHT -> direction = when (instruction.value) {
+                    90 -> direction.turnRight()
+                    180 -> direction.turnAround()
+                    270 -> direction.turnLeft()
                     else -> throw IllegalStateException("Could not turn right by ${instruction.value}°")
                 }
             }
         }
 
-        fun moveWithWaypoints(instruction: Instruction) {
-            when(instruction.action) {
-                Action.FORWARD -> position += waypoint * instruction.value
-                Action.NORTH -> waypoint = Point(waypoint.x, waypoint.y + instruction.value)
-                Action.EAST -> waypoint = Point(waypoint.x + instruction.value, waypoint.y )
-                Action.SOUTH -> waypoint = Point(waypoint.x, waypoint.y - instruction.value )
-                Action.WEST -> waypoint = Point(waypoint.x - instruction.value, waypoint.y )
-                Action.LEFT -> when(instruction.value) {
-                    90 -> waypoint = waypoint.turnLeft()
-                    180 -> waypoint = waypoint.turnAround()
-                    270 -> waypoint = waypoint.turnRight()
-                    else -> throw IllegalStateException("Could not turn left by ${instruction.value}°")
-                }
-                Action.RIGHT -> when(instruction.value) {
-                    90 -> waypoint = waypoint.turnRight()
-                    180 -> waypoint = waypoint.turnAround()
-                    270 -> waypoint = waypoint.turnLeft()
-                    else -> throw IllegalStateException("Could not turn right by ${instruction.value}°")
-                }
-            }
-        }
         fun manhattanDistance(): Int {
             return abs(position.x) + abs(position.y)
         }
     }
+
     data class Point(val x: Int = 0, val y: Int = 0) {
         operator fun plus(p: Point) = Point(x + p.x, y + p.y)
-        operator fun times(i: Int) =  Point(x * i, y * i)
+        operator fun times(i: Int) = Point(x * i, y * i)
 
         fun turnLeft(): Point {
             return Point(-y, x)
         }
+
         fun turnRight(): Point {
             return Point(y, -x)
         }
+
         fun turnAround(): Point {
             return Point(-x, -y)
         }
     }
-    enum class Direction(val x: Int, val y: Int) {
-        NORTH(0, 1),
-        EAST(1, 0),
-        SOUTH(0, -1),
-        WEST(-1, 0);
 
-        operator fun times(i: Int): Point = Point(x*i, y*i)
+    enum class Direction(val point: Point) {
+        NORTH(Point(0, 1)),
+        EAST(Point(1, 0)),
+        SOUTH(Point(0, -1)),
+        WEST(Point(-1, 0));
 
-        fun turnLeft(): Direction {
-            return when(this) {
-                NORTH -> WEST
-                EAST -> NORTH
-                SOUTH -> EAST
-                WEST -> SOUTH
-            }
-        }
-        fun turnRight(): Direction {
-            return when(this) {
-                NORTH -> EAST
-                EAST -> SOUTH
-                SOUTH -> WEST
-                WEST -> NORTH
-            }
-        }
-        fun turnAround(): Direction {
-            return when(this) {
-                NORTH -> SOUTH
-                EAST -> WEST
-                SOUTH -> NORTH
-                WEST -> EAST
-            }
-        }
+        operator fun times(i: Int) = point * i
     }
+
     data class Instruction(val action: Action, val value: Int)
     enum class Action {
         NORTH,
@@ -127,13 +95,14 @@ class Day12(inputFile:String = "d12"): Day(inputFile) {
         RIGHT,
         FORWARD
     }
+
     companion object {
         private fun toInstruction(input: String): Instruction {
             return Instruction(toAction(input[0]), input.drop(1).toInt());
         }
 
         private fun toAction(char: Char): Action {
-            return when(char) {
+            return when (char) {
                 'N' -> Action.NORTH
                 'S' -> Action.SOUTH
                 'E' -> Action.EAST
