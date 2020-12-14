83f87313bbc31e86ac5a827138e12034310a573f
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2020-12-14 09:35:52 +0100

    Add output

--- src/main/kotlin/be/janvanryckeghem/aoc2020/Runner.kt
+++ src/main/kotlin/be/janvanryckeghem/aoc2020/Runner.kt
@@ -5,7 +5,7 @@ import kotlin.system.measureTimeMillis
 val puzzles: List<Day> = listOf(
     Day01(),
     Day02(),
-    Day03(), Day03("d03-kris"),
+    Day03(),
     Day04(),
     Day05(),
     Day06(),
@@ -20,15 +20,18 @@ val puzzles: List<Day> = listOf(
 )
 
 fun main() {
-    var solution: Any? = null
-    var runTime: Long
-    puzzles.forEach { puzzle ->
-        println("--- ${puzzle.DAY} ---")
-        runTime = measureTimeMillis { solution = puzzle.part1() }
-        println("> Part 1:\t$solution")
-        println("  > in ${runTime}ms")
-        runTime = measureTimeMillis { solution = puzzle.part2() }
-        println("> Part 2:\t$solution")
-        println("  > in ${runTime}ms")
+    val totalRuntime = measureTimeMillis {
+        var solution: Any? = null
+        var runTime: Long
+        puzzles.forEach { puzzle ->
+            println("--- ${puzzle.DAY} ---")
+            runTime = measureTimeMillis { solution = puzzle.part1() }
+            println("> Part 1:\t$solution")
+            println("  > in ${runTime}ms")
+            runTime = measureTimeMillis { solution = puzzle.part2() }
+            println("> Part 2:\t$solution")
+            println("  > in ${runTime}ms")
+        }
     }
+    println("\nRan all puzzles in ${totalRuntime}ms")
 }
