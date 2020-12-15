ffd00ba347476a6c2386c2834c8ed08307af1692
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2020-12-15 09:04:26 +0100

    15: small refactorings

--- src/main/kotlin/be/janvanryckeghem/aoc2020/Day15.kt
+++ src/main/kotlin/be/janvanryckeghem/aoc2020/Day15.kt
@@ -1,7 +1,5 @@
 package be.janvanryckeghem.aoc2020
 
-import java.lang.IllegalStateException
-
 class Day15(inputFile: String = "d15") : Day(inputFile) {
     override val DAY: String = "DAY 15"
     override val parseInput: (String) -> List<Int> = { input -> input.split(",").map { it.toInt() } }
@@ -15,14 +13,13 @@ class Day15(inputFile: String = "d15") : Day(inputFile) {
     }
 
     fun memoryGame(input: List<Int>, turns: Int = 2020): Int {
-        val memory = mutableMapOf<Int, Int>()
-        var last: Int? = null
-        repeat(turns - 1) { turn ->
-            val current = input.elementAtOrElse(turn) { last!! }
-
+        val memory = input.withIndex().associateBy({ it.value }, { it.index + 1 }).toMutableMap()
+        var last = input.last()
+        for (turn in memory.size until turns) {
+            val current = last
             last = if (current in memory) turn - memory[current]!! else 0
             memory[current] = turn
         }
-        return last!!
+        return last
     }
 }
